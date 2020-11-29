26015e435550ecbbbc1e193d89718659dcbb030a
damienen <damian.ovidiu27@gmail.com>
2020-11-29 22:24:18 +0200

    Add week 2 projects

---
+++ lib/numberShapes.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:powers/powers.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  TextEditingController numberInput = TextEditingController();
+
+// This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Number Shapes',
+        home: Scaffold(
+            appBar: AppBar(title: const Text('Number Shapes')),
+            body: Column(
+              children: <Widget>[
+                const Padding(
+                    padding: EdgeInsets.all(5),
+                    child: Text(
+                        'Please input a number to see if it is a square or a triangular')),
+                Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.allow(RegExp(r'^[0-9]*$'))
+                      ],
+                      style: const TextStyle(fontSize: 20, color: Colors.black),
+                      textAlign: TextAlign.center,
+                      controller: numberInput,
+                    ))
+              ],
+            ),
+            floatingActionButton: FloatingActionButton.extended(
+                onPressed: () {
+                  if (numberInput.text.isNotEmpty) {
+                    int nr = int.parse(numberInput.text);
+                    String alertText = '';
+                    if (nr.isSquare) alertText += '$nr is a SQUARE';
+                    if (nr.isCube) alertText += '\n$nr is a CUBE';
+                    showDialog<void>(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                              title: Text(alertText.isEmpty
+                                  ? '$nr is neither a CUBE or a SQUARE'
+                                  : alertText));
+                        });
+                    numberInput.clear();
+                  }
+                },
+                elevation: 5,
+                icon: const Icon(Icons.where_to_vote_rounded),
+                backgroundColor: Colors.blueGrey,
+                splashColor: Colors.blue, label:const Text('CALCULATE'),)));
+  }
+}
---
+++ lib/playground.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:meta/meta.dart';
+
+Future<void> main() async {
+  const String url = 'https://yts.mx/api/v2/list_movies.json';
+  final Response response = await get(url);
+  final Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+  final List<dynamic> movieList = jsonResponse['data']['movies'];
+
+
+  //print(result.toString());
+}
+
+class Movie {
+  const Movie({@required this.title,
+    @required this.year,
+    @required this.summary,
+    @required this.genres});
+
+  Movie.fromJson(dynamic item)
+      :title=item['title'],
+        year=item['year'],
+        summary=item['summary'],
+        genres=List<String>.from(item['genres']);
+
+  final String title;
+  final int year;
+  final String summary;
+  final List<String> genres;
+
+  @override
+  String toString() {
+    return 'Movie{title: $title, year: $year, summary: $summary, genres: $genres}';
+  }
+}
+
+/*
+void main() async {
+  const url='https://yts.mx/api/v2/list_movies.json';
+  get(url).then((Response response){
+    Map<String, dynamic> map = jsonDecode(response.body);
+    print(map['status']);
+  });
+
+}
+*/
+/*for (int i = 0; i < movieList.length; i++) {
+    print(movieList[i]['title']);
+  }*/
