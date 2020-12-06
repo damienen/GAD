06acdd2e3d6202d26ded908db8552147aa0126e8
damienen <damian.ovidiu27@gmail.com>
2020-12-06 06:28:26 +0200

    Solved analyzer errors (or at least I hope so)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'file:///D:/GAD_Flutter/currency_convertor/lib/01/main.dart';
+import 'file:///D:/GAD_Flutter/currency_convertor/lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
