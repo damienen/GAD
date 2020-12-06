2fbbc438158074b6c725d70f663ec1ba89de28b5
damienen <damian.ovidiu27@gmail.com>
2020-12-06 06:34:47 +0200

    Changed widget_test.dart path to tested file from absolute to relative

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'file:///D:/GAD_Flutter/currency_convertor/lib/main.dart';
+import 'package:currency_convertor/01/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
